c65cc4b424a6b89e53db97f80430a5046dc02d13
tonybarr1611 <antbarrantes@estudiantec.cr>
2025-01-16 18:54:52 -0600

    Routes
    Routes to navigate between pages as needed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { GameComponent } from './game/game/game.component';
+import { ScoreComponent } from './game/score/score.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'play', component: GameComponent },
+  { path: 'score/:score', component: ScoreComponent },
+];
